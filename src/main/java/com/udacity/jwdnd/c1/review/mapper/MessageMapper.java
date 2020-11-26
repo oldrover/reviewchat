46b6359650d3e44d13396e64fc8f6c86ce1ea39d
oldrover <oldrover@web.de>
2020-11-26 14:48:48 +0100

    created MessageMapper

---
+++ src/main/java/com/udacity/jwdnd/c1/review/mapper/MessageMapper.java
@@ -0,0 +1,17 @@
+package com.udacity.jwdnd.c1.review.mapper;
+
+import com.udacity.jwdnd.c1.review.model.ChatMessage;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface MessageMapper {
+    @Select("SELECT * FROM MESSAGES WHERE username = #{username}")
+    ChatMessage getMessage(String username);
+
+    @Insert("INSERT INTO MESSAGES(username, messagetext) VALUES(#{username}, #{messagetext})")
+    @Options(useGeneratedKeys = true, keyProperty = "messageId")
+    int insert(ChatMessage chatMessage);
+}
