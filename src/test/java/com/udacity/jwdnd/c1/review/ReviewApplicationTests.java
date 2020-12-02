b18b462513cc1151595ad020dd759a0f08f9fefc
oldrover <oldrover@web.de>
2020-12-02 08:48:21 +0100

    created testuser class

--- src/test/java/com/udacity/jwdnd/c1/review/ReviewApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/c1/review/ReviewApplicationTests.java
@@ -1,13 +1,58 @@
 package com.udacity.jwdnd.c1.review;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReviewApplicationTests {
+	@LocalServerPort
+	private Integer port;
+
+	private static WebDriver driver;
+	private ChatPage chat;
+	private LoginPage login;
+	private SignupPage signup;
+
+	@BeforeAll
+	public static void beforeAll() {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		driver.quit();
+	}
 
 	@Test
-	void contextLoads() {
+	public void signupTest() throws InterruptedException {
+		driver.get("http://localhost:" + port + "/signup");
+		signup = new SignupPage(driver);
+		signup.setFirstName("firstname");
+		signup.setLastName("lastname");
+		signup.setUsername("username");
+		signup.setPassword("password");
+		signup.clickSubmitButton();
+		Thread.sleep(2000);
+		signup.clickLoginLink();
+		loginTest();
 	}
+
+	@Test
+	public void loginTest() {
+		login = new LoginPage(driver);
+
+
+	}
+
+
+
 
 }
